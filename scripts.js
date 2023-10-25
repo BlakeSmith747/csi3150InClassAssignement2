71b2fea391a658e6d72acd418a280067d5d67344
BlakeSmith747 <BlakeSmith747@users.noreply.github.com>
2023-10-24 23:52:36 -0400

    Initial commit from W3Schools

---
+++ scripts.js
@@ -0,0 +1,41 @@
+function showImgA() {
+  let imgA = document.querySelector(".imgA");
+  imgA.style.visibility = "visible";
+  let hide_imgA = document.querySelector(".hide_imgA");
+  hide_imgA.style.visibility = "visible";
+}
+
+function hideImgA() {
+  let imgA = document.querySelector(".imgA");
+  imgA.style.visibility = "hidden";
+  let hide_imgA = document.querySelector(".hide_imgA");
+  hide_imgA.style.visibility = "hidden";
+}
+
+function showImgB() {
+  let imgB = document.querySelector(".imgB");
+  imgB.style.visibility = "visible";
+  let hide_imgB = document.querySelector(".hide_imgB");
+  hide_imgB.style.visibility = "visible";
+}
+
+function hideImgB() {
+  let imgB = document.querySelector(".imgB");
+  imgB.style.visibility = "hidden";
+  let hide_imgB = document.querySelector(".hide_imgB");
+  hide_imgB.style.visibility = "hidden";
+}
+
+function showImgC() {
+  let imgC = document.querySelector(".imgC");
+  imgC.style.visibility = "visible";
+  let hide_imgC = document.querySelector(".hide_imgC");
+  hide_imgC.style.visibility = "visible";
+}
+
+function hideImgC() {
+  let imgC = document.querySelector(".imgC");
+  imgC.style.visibility = "hidden";
+  let hide_imgC = document.querySelector(".hide_imgC");
+  hide_imgC.style.visibility = "hidden";
+}
